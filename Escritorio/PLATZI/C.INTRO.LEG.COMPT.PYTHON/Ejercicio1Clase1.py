1b1692689c39b8b3d3acf542ca040dc150827d82
ZzCristianzZ <crisfegomez@gmail.com>
2022-11-03 14:20:00 -0500

    Se agrego el primer ejercicio

---
+++ Escritorio/PLATZI/C.INTRO.LEG.COMPT.PYTHON/Ejercicio1Clase1.py
@@ -0,0 +1,6 @@
+# Programar el pedido de tu nombre, y que este te salude y al mismo tiempo te mencione cuantos caracteres tiene la oracion
+
+nombre = input('Cual es tu nombre: ')
+
+saludo = f'Hola {nombre}, un placer conocerte '
+print (saludo + 'la longitud de este texto en numero de caracteres es ', len(saludo))
