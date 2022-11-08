a1c4445d612f4dc6df99e741ebb1ca663655cbd6
ZzCristianzZ <crisfegomez@gmail.com>
2022-11-08 15:44:30 -0500

    Proyecto final del modulo de Algoritmos y funciones

---
+++ Escritorio/PLATZI/C.INTRO.LEG.COMPT.PYTHON/Raiz_cuadrada_funciones.py
@@ -0,0 +1,64 @@
+def opciones():
+    print(f'Opciones para hallar raiz cuadrada \n (1)Enumeracion Exhaustiva \n (2) aproximacion de soluciones \n (3) Busqueda Binaria')
+
+    opcion = int(input('Elija una opcion: '))
+    numero = int(input('Elija un numero: '))
+
+    if opcion == 1:
+        Enumeracion(numero)
+    elif opcion == 2:
+        Aproximacion(numero)
+    elif opcion == 3:
+        Busquedabinaria(numero)
+    else:
+        print('Elija 1, 2 o 3')
+
+def Enumeracion(objetivo):
+    respuesta = 0
+
+    while respuesta**2 < objetivo:
+            print(respuesta)
+            respuesta += 1
+    
+    if respuesta**2 == objetivo:
+        print(f'La raiz cuadrada de {objetivo} es {respuesta}')
+    else:
+        print(f'{objetivo} no tiene una raiz cuadrada exacta')
+
+def Aproximacion(objetivo):
+    epsilon = 0.001
+    paso = epsilon**2
+    respuesta = 0.0
+
+    while abs (respuesta**2 - objetivo) >= epsilon and respuesta <= objetivo:
+        respuesta += paso 
+    
+    if abs(respuesta**2 - objetivo) >= epsilon:
+        print (f'No se encontro la raiz cuadrada {objetivo}')
+    else:
+        print ('La raiz cuadrada de {objetivo} es aproximadamente {respuesta}')
+
+def Busquedabinaria(objetivo):
+    epsilon = 0.001
+    bajo = 0.0
+    alto = max(1.0, objetivo)
+    respuesta = (alto + bajo)/2
+
+    while abs (respuesta**2 - objetivo) >= epsilon:
+        print(f'bajo={bajo}, alto={alto}, respuesta={respuesta}')
+
+        if respuesta**2 < objetivo:
+            bajo = respuesta
+        else:
+            alto = respuesta
+
+        respuesta = (alto + bajo)/2
+    print(f'La raiz cuadrada de {objetivo} es {respuesta}')
+
+opciones()
+
+
+    
+
+
+
